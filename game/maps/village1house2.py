0a1e833eaeb93e5d5b2ef6c2647b41e1369768aa
gjgfuj <gjgfujb@gmail.com>
2014-09-07 21:59:34 +1000

    Added things I forgot to add.

---
+++ game/maps/village1house2.py
@@ -0,0 +1,21 @@
+m = [
+  ["#","#","#","#","#"],
+  ["#"," "," "," ","#"],
+  ["#"," "," "," ","#"],
+  ["#"," "," "," ","#"],
+  ["#","#"," ","#","#"],
+]
+bindings = {
+  "#": "brickwall.png",
+  " ": "brickfloor.png"
+}
+movement = {
+  "#": False,
+  " ": True
+}
+events = [
+  {"type": "player", "position": (2,3), "sprite": "player.png"},
+  {"type": "level", "position": (2,4), "level": "village1", "newposition": (10, 17)},
+]
+scripts = [
+]
